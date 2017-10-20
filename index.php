8b5dfcdf8ca42f5d44aa7fb9ce3f69386fff790f
akash r asli <ara59@afsconnect2.njit.edu>
2017-10-19 22:48:52 -0400

    URLs fixed,Static functions included

--- index.php
+++ index.php
@@ -30,12 +30,12 @@ class main {
         }
         
         //instantiate the class that is being requested
-         $page = new $pageRequest;          
+        $page = new $pageRequest;          
 
         if($_SERVER['REQUEST_METHOD'] == 'GET') {            
-            $page->get();
-            
-        } else {            
+            $page->get();            
+        } 
+        else {            
             $page->post();
         }
     }
@@ -46,91 +46,80 @@ class main {
 abstract class page {
     protected $html;
 
-    public function __construct()
-    {        
+    public function __construct(){        
         $this->html .= '<html>';        
         $this->html .= '<body>';
     }
-    public function __destruct()
-    {
+    public function __destruct(){
         $this->html .= '</body></html>';        
-        printOutput::printHtml($this->html);
+        die($this->html);
     }
 }
 
-class printOutput
-{
-    static public function printHtml($inputHtml){
-    return print($inputHtml);
-    }
-} 
-
-
-class uploadform extends page
-{
-    //Creates a form to upload files
-    public function get()
-    {
+class htmlTag{
+    //Displays form to upload file
+    static public function uploadForm(){
         $form='<form action="index.php?page=uploadform" method="post" enctype="multipart/form-data">';      
         $form.='Select file to upload:';
         $form.='<input type="file" name="fileToUpload" id="fileToUpload">';
         $form.='<input type="submit" value="Upload" name="submit">';
-        $form.='</form>';        
-        $this->html .= $form;
+        $form.='</form>';
+        return $form;
+    }
+    
+    //Generates HTML Table From CSV File
+    static public function generateHtml(){
+        $linecount = 0;
+        //Opens CSV File from AFS Server
+        $myfile = fopen($_GET['filename'], "r") or die("Unable to open file!");
 
+        $html='';
+        $html='<table border="1">';
+        //Create HTML till end of CSV file
+        while(!feof($myfile)){
+            $html.='<tr>';
+            //Gets lines from File
+            $line = (array) fgetcsv($myfile);
+            //Creates data cell
+            foreach($line as $value){          
+              if($linecount==0)
+              $html.='<th>'.$value.'</th>';
+            else
+              $html.='<td>'.$value.'</td>';
+            } 
+          
+          $linecount++;
+          $html.='</tr>';
+          }      
+        $html.='</table>';              
+        fclose($myfile);
+        return $html;
+      }
+}
+
+class uploadform extends page
+{
+    //Creates a form to upload files
+    public function get(){                
+        $this->html .= htmlTag::uploadForm();
     }
     //Uploads form to AFS Server
-    public function post()
-    {
-        $target_dir = "/afs/cad/u/a/r/ara59/public_html/project1/uploads/";
+    public function post(){
+        $target_dir = "uploads/";
         $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
         $uploadOk = 1;
-
-        if ($uploadOk == 0) {
-              $this->html .="Sorry, your file was not uploaded.";        
-        } 
-        // if everything is ok, try to upload file
-        else {
-              if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {        
-              header('Location: https://web.njit.edu/~ara59/project1/index.php?page=displayHtml&filename='.$target_file);
-        } 
-        else {
-            $this->html .="Sorry, there was an error uploading your file.";
-        }
+        
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {        
+        header('Location: index.php?page=displayHtml&filename='.$target_file);
+               
         }
     }
 }
-
 //Display the output from CSV file to HTML
 class displayHtml extends page
 {
     public function get(){
-      $linecount = 0;
-      //Opens CSV File from AFS Server
-      $myfile = fopen($_GET['filename'], "r") or die("Unable to open file!");
-
-      $html='';
-      $html='<table border="1">';
-      //Create HTML till end of CSV file
-      while(!feof($myfile)){
-        $html.='<tr>';
-        //Gets lines from File
-        $line = fgets($myfile);
-        $string_length=strlen($line);               
-        $array1= explode ( ',', $line);
-        //Creates data cell
-        foreach($array1 as $value){          
-          if($linecount==0)
-            $html.='<th>'.$value.'</th>';
-          else
-            $html.='<td>'.$value.'</td>';
-          } 
-        $linecount++;
-        $html.='</tr>';
-      }      
-      $html.='</table>';
-      $this->html .= $html;      
-      fclose($myfile);
+      $this->html.=htmlTag::generateHtml();
     }
 }
 ?>
